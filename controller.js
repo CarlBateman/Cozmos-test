114c693f75392b3af4b4627a63922e9964946d23
CarlBateman <CarlBateman@hotmailcom>
2024-06-28 13:24:55 +0100

    Moved load texture code to own module

--- controller.js
+++ controller.js
@@ -7,7 +7,7 @@ import { OutlinePass } from 'three/addons/postprocessing/OutlinePass.js';
 import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 import { FXAAShader } from 'three/addons/shaders/FXAAShader.js';
 
-//import { getImageOrVideoTexture } from './loadtexture.js';
+import { getImageOrVideoTexture } from './loadtexture.js';
 
 let camera, scene, renderer;
 let pointer = new THREE.Vector2();
@@ -69,79 +69,6 @@ function setup() {
 	effectFXAA = new ShaderPass(FXAAShader);
 	effectFXAA.uniforms['resolution'].value.set(1 / window.innerWidth, 1 / window.innerHeight);
 	composer.addPass(effectFXAA);
-}
-
-function isValidUrl(string) {
-	try {
-		new URL(string);
-		return true;
-	} catch (err) {
-		return false;
-	}
-}
-
-function checkUrlExists(txtURL) {
-	return fetch(txtURL, { method: 'HEAD' })
-		.then(
-			function (response) {
-				return response.ok;
-			})
-		.catch(function (error) {
-			return false;
-		});
-}
-
-function getVideoTexture(txtURL) {
-	const videoElement = document.createElement("video");
-	videoElement.crossOrigin = "anonymous";
-	videoElement.src = txtURL;
-	videoElement.load();
-	videoElement.controls = true;
-	videoElement.play();
-
-	return new Promise(function (resolve) {
-		videoElement.addEventListener("loadedmetadata", function (e) {
-			const ratio = this.videoHeight / this.videoWidth;
-			let texture = new THREE.VideoTexture(videoElement);
-			resolve({ texture, ratio });
-		}, false);
-	});
-}
-
-function getImageTexture(txtURL) {
-	const loader = new THREE.TextureLoader();
-	return loader.loadAsync(txtURL).then(
-		function (texture) {
-			const ratio = texture.image.height / texture.image.width;
-			return { texture, ratio };
-		});
-}
-
-function getImageOrVideoTexture(txtURL) {
-	return new Promise(function (resolve) {
-
-		if (isValidUrl(txtURL)) {
-			checkUrlExists(txtURL).then(function (exists) {
-				if (exists) {
-
-					return fetch(txtURL).then(
-						function (response) {
-							const type = (response.headers.get("Content-Type"));
-							switch (true) {
-								case type.includes("video"):
-									resolve( getVideoTexture(txtURL));
-									break;
-								case type.includes("image"):
-									resolve( getImageTexture(txtURL));
-									break;
-								default:
-							}
-						})
-
-				}
-			});
-		}
-	});
 }
 
 function addTexturedMesh(e) {
---
+++ loadtexture.js
@@ -0,0 +1,80 @@
+﻿import * as THREE from 'three';
+
+
+function isValidUrl(string) {
+	try {
+		new URL(string);
+		return true;
+	} catch (err) {
+		return false;
+	}
+}
+
+function checkUrlExists(txtURL) {
+	return fetch(txtURL, { method: 'HEAD' })
+		.then(
+			function (response) {
+				return response.ok;
+			})
+		.catch(function (error) {
+			return false;
+		});
+}
+
+function getVideoTexture(txtURL) {
+	const videoElement = document.createElement("video");
+	videoElement.crossOrigin = "anonymous";
+	videoElement.src = txtURL;
+	videoElement.load();
+	videoElement.controls = true;
+	videoElement.play();
+
+	return new Promise(function (resolve) {
+		videoElement.addEventListener("loadedmetadata", function (e) {
+			const ratio = this.videoHeight / this.videoWidth;
+			let texture = new THREE.VideoTexture(videoElement);
+			resolve({ texture, ratio });
+		}, false);
+	});
+}
+
+function getImageTexture(txtURL) {
+	const loader = new THREE.TextureLoader();
+	return loader.loadAsync(txtURL).then(
+		function (texture) {
+			const ratio = texture.image.height / texture.image.width;
+			return { texture, ratio };
+		});
+}
+
+function getImageOrVideoTexture(txtURL) {
+	return new Promise(function (resolve) {
+
+		if (isValidUrl(txtURL)) {
+			checkUrlExists(txtURL).then(function (exists) {
+				if (exists) {
+
+					return fetch(txtURL).then(
+						function (response) {
+							const type = (response.headers.get("Content-Type"));
+							switch (true) {
+								case type.includes("video"):
+									resolve(getVideoTexture(txtURL));
+									break;
+								case type.includes("image"):
+									resolve(getImageTexture(txtURL));
+									break;
+								default:
+							}
+						})
+
+				}
+			});
+		}
+	});
+}
+
+
+
+
+export { getImageOrVideoTexture };
